f283125b2fa8495b6d39dc165bd76131d507ff0d
luciomelo1407 <luciomelo90@gmail.com>
2026-03-09 15:43:58 -0300

    rebegin the project with java security

---
+++ src/main/java/com/luciomelo90/pomotask/PomotaskApplication.java
@@ -0,0 +1,13 @@
+package com.luciomelo90.pomotask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomotaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomotaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luciomelo90/pomotask/PomotaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciomelo90.pomotask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomotaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
